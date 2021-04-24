f223cb49ab6bfe8eaeb0f145806ac87757a82a9e
Alex003Alex <79718105+Alex003Alex@users.noreply.github.com>
2021-04-24 19:32:57 +0300

    Add files via upload

---
+++ DZ 5.7/ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,29 @@
+﻿//Посчитать количество элементов массива, больших заданного числа
+#include "pch.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+
+	int i, arr[10], b = 0, n;
+
+	for (i = 0; i < 10; i++)
+		arr[i] = rand() % 11;
+
+	cout << "Введите число ";
+	cin >> n;
+
+	for (i = 0; i < 10; i++) {
+		cout << arr[i] << " ";
+		if (arr[i] > n)
+			b++;
+	}
+
+	cout << endl << "bolshe "<< n <<" = " << b << endl;
+
+}
---
+++ DZ 5.8/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿//Посчитать количество элементов массива, больших заданного числа
+#include "pch.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+
+	int i, arr1[10], arr2[10],arr3[10];
+
+	for (i = 0; i < 10; i++) {
+		arr1[i] = rand() % 11;
+		cout << arr1[i] << " ";
+	}
+	cout << endl;
+	for (i = 0; i < 10; i++){
+		arr2[i] = rand() % 11;
+		cout << arr2[i] << " ";
+}
+	cout << endl;
+	for (i = 0; i < 10; i++){
+		arr3[i] = arr1[i] * arr2[i];
+		cout << arr3[i] << " ";
+}
+}
